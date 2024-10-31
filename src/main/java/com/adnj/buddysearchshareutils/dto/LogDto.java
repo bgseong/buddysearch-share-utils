26ec41fa9b349bac3b218707ed93f773e98fca1f
bgsung <qnrms2898@naver.com>
2024-10-31 15:57:08 +0900

    feat: 0.0.22

--- src/main/java/com/adnj/buddysearchshareutils/dto/LogDto.java
+++ src/main/java/com/adnj/buddysearchshareutils/dto/LogDto.java
@@ -1,13 +1,15 @@
 package com.adnj.buddysearchshareutils.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.util.UUID;
 
 public class LogDto {
 
-    @Data
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Builder
     public static class Report{
         private UUID reporterId;
